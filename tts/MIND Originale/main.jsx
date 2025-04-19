d294341ab765702814b5c0ca800505111c47263c
Narion2025 <Ben.Poersch@diyrigent.de>
2025-04-20 00:50:52 +0200

    hume

---
+++ tts/MIND Originale/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CallAgent from './call_agent_component.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CallAgent />
+  </React.StrictMode>
+);
